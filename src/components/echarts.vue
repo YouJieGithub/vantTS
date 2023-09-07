57cf68863d83e8db68190fa51e7313ead35ac62d
YouJieGithub <707677807@qq.com>
2023-09-07 15:34:29 +0800

    vantTS

--- src/components/echarts.vue
+++ src/components/echarts.vue
@@ -1,12 +1,30 @@
 <template>
-  <v-chart :option="props.option" autoresize class="chart"/>
+  <v-chart :option="option" autoresize class="chart"/>
 </template>
 
 <script lang="ts" setup>
+import VChart from "vue-echarts";
+import {use} from "echarts/core";
+import {ECBasicOption} from 'echarts/types/dist/shared'
+import {CanvasRenderer} from "echarts/renderers";
+import {BarChart, LineChart, PieChart} from "echarts/charts";
+import {DatasetComponent, GridComponent, LegendComponent, TitleComponent, TooltipComponent,} from "echarts/components";
 
-const props = defineProps({
-  option: String
-})
+use([
+  BarChart,
+  LineChart,
+  PieChart,
+  CanvasRenderer,
+  GridComponent,
+  TooltipComponent,
+  TitleComponent,
+  LegendComponent,
+  DatasetComponent,
+]);
+
+defineProps<{
+  option: ECBasicOption | undefined
+}>()
 </script>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -6,27 +6,11 @@ import "amfe-flexible";
 import "@/assets/css/index.css"; //公共样式
 import "vant/lib/index.css"; //vant样式
 import "@/assets/vantBase/index.css"; //vant 基础变量自定义样式
-import VueECharts from "vue-echarts";
-import {use} from "echarts/core";
-import {CanvasRenderer} from "echarts/renderers";
-import {BarChart, LineChart, PieChart} from "echarts/charts";
-import {DatasetComponent, GridComponent, LegendComponent, TitleComponent, TooltipComponent,} from "echarts/components";
 import {Notify, setNotifyDefaultOptions} from "vant";
 import dayjs from "dayjs";
 import "dayjs/locale/zh-cn";
 import '@/assets/font/iconfont';
 
-use([
-  BarChart,
-  LineChart,
-  PieChart,
-  CanvasRenderer,
-  GridComponent,
-  TooltipComponent,
-  TitleComponent,
-  LegendComponent,
-  DatasetComponent,
-]);
 
 setNotifyDefaultOptions({
   className: 'setDefaultNotify'
@@ -48,5 +32,4 @@ createApp(App)
     .use(router)
     .use(store, key)
     .use(Notify)
-    .component("v-chart", VueECharts)
     .mount("#app");
